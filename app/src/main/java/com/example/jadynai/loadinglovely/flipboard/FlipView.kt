0d4d54cde4c8070fc086aa7359df62611a75dd91
AiLoli <aa>
2018-06-11 23:56:05 +0800

    1.0版本 完成单页上下翻的逻辑

--- app/src/main/java/com/example/jadynai/loadinglovely/flipboard/FlipView.kt
+++ app/src/main/java/com/example/jadynai/loadinglovely/flipboard/FlipView.kt
@@ -22,9 +22,23 @@ class FlipView(context: Context, attributes: AttributeSet) : View(context, attri
     val girls = arrayListOf(R.drawable.girl_0, R.drawable.girl_1, R.drawable.girl_2,
             R.drawable.girl_3, R.drawable.girl_4, R.drawable.girl_5, R.drawable.girl_6)
 
+    init {
+        setLayerType(LAYER_TYPE_SOFTWARE, null)
+    }
 
-    private var startX: Float? = 0f
-    private var startY: Float? = 0f
+
+    private var startX: Float = 0f
+    private var startY: Float = 0f
+
+    //向下翻旋转角度
+    private var rotateF = 180f
+    //向上翻旋转角度
+    private var rotateS = 0f
+
+    //是否移动上半部分0为松手，1为向下翻，-1为向上翻
+    private var statusFlip = 0
+
+    private val TAG = "cece"
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         when (event?.actionMasked) {
@@ -35,11 +49,34 @@ class FlipView(context: Context, attributes: AttributeSet) : View(context, attri
             MotionEvent.ACTION_MOVE -> {
                 val x = event?.x
                 val y = event?.y
+                //当y运动距离大于x的1.5倍时，才判断为垂直翻动
+                val disY = y - startY
+                if (Math.abs(disY) > 1f && Math.abs(disY) >= Math.abs(x - startX) * 1.5f) {
+                    if (statusFlip == 0) {
+                        statusFlip = if (disY > 0) 1 else -1
+                    }
+                    val ratio = Math.abs(disY) / centerY
+                    if (statusFlip == 1) {
+                        //向下翻
+                        rotateF = (1 - ratio) * 180f
+                    } else {
+                        rotateS = ratio * 180f
+                    }
+                    invalidate()
+                }
             }
             MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                resetData(event)
             }
         }
         return true
+    }
+
+    private fun resetData(event: MotionEvent?) {
+        rotateF =180f
+        rotateS = 0f
+        statusFlip = 0
+        invalidate()
     }
 
     private val camera by lazy {
@@ -74,16 +111,17 @@ class FlipView(context: Context, attributes: AttributeSet) : View(context, attri
     * */
     fun drawFirstHalf(canvas: Canvas?) {
         canvas?.save()
-        camera.save()
         canvas?.clipRect(0, 0, width, height / 2)
-        camera.rotateX(180f)
+        camera.save()
+//        drawMatrix.reset()
+        camera.rotateX(rotateF)
         camera.rotateY(180f)
         camera.getMatrix(drawMatrix)
         camera.restore()
         drawMatrix.preTranslate(-centerX, 0f)
         drawMatrix.postTranslate(centerX, centerY)
         //高度变矮
-//        drawMatrix.preScale(1.0f, 0.6f)
+        drawMatrix.preScale(1.0f, rotateF / 180f)
         //这里也可以旋转canvas，性能上无差别
         drawMatrix.preRotate(180f, centerX, centerY / 2)
         //or旋转Canvas
@@ -99,15 +137,14 @@ class FlipView(context: Context, attributes: AttributeSet) : View(context, attri
     fun drawSecondHalf(canvas: Canvas?) {
         canvas?.save()
         camera.save()
-        canvas?.clipRect(0, height / 2, width, height)
-        camera.rotateX(0f)
+        camera.rotateX(rotateS)
         camera.getMatrix(drawMatrix)
         camera.restore()
         drawMatrix.preTranslate(-centerX, 0f)
-        drawMatrix.postTranslate(centerX, 0f)
+        drawMatrix.postTranslate(centerX, centerY)
         //高度变矮
-//        drawMatrix.preScale(1.0f, 1 - (rotate / 90f))
-        canvas?.drawBitmap(bitmap, drawMatrix, null)
+        drawMatrix.preScale(1.0f, (90f - rotateS) / 90f)
+        canvas?.drawBitmap(BitmapUtils.cropSaveSecondHalf(bitmap), drawMatrix, null)
         canvas?.restore()
     }
 }
