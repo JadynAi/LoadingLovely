c707bf7692e3778fb40ea35c00b4977dcea75374
AiLoli <aa>
2018-06-26 23:52:50 +0800

    1.0版本 DebugView学习Matrix的测试代码提交

--- app/src/main/java/com/example/jadynai/loadinglovely/flipboard/DebugView.kt
+++ app/src/main/java/com/example/jadynai/loadinglovely/flipboard/DebugView.kt
@@ -19,23 +19,22 @@ import com.example.jadynai.loadinglovely.R
  */
 class DebugView(context: Context, attributes: AttributeSet) : View(context, attributes) {
 
-    val girls = arrayListOf(R.drawable.girl_0, R.drawable.girl_1, R.drawable.girl_2,
-            R.drawable.girl_3, R.drawable.girl_4, R.drawable.girl_5, R.drawable.girl_6)
+    val girls = arrayListOf(R.drawable.girl_0)
 
 
     private val camera by lazy {
         Camera()
     }
 
-    private val drawMatrix by lazy {
+    val drawMatrix by lazy {
         Matrix()
     }
 
-    private val centerX by lazy {
+    val centerX by lazy {
         width / 2.toFloat()
     }
 
-    private val centerY by lazy {
+    val centerY by lazy {
         height / 2.toFloat()
     }
 
@@ -47,49 +46,16 @@ class DebugView(context: Context, attributes: AttributeSet) : View(context, attr
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         //绘制当前页
-        drawFirstHalf(canvas, curBitmap, 0f)
-        drawSecondHalf(canvas, curBitmap, 0f)
-
+        drawFirstHalf(canvas, curBitmap)
     }
 
     /*
     * 绘制上半部分，以及上半部分的变化。
     * 上半部分角度由180 变化到 90，递减
     * */
-    fun drawFirstHalf(canvas: Canvas?, bitmap: Bitmap?, rotate: Float) {
+    fun drawFirstHalf(canvas: Canvas?, bitmap: Bitmap?) {
         bitmap?.apply {
             canvas?.save()
-            canvas?.clipRect(0, 0, width, height / 2)
-            camera.save()
-            camera.rotateX(0f)
-            camera.getMatrix(drawMatrix)
-            camera.restore()
-            drawMatrix.preScale(1.0f, 1.0f)
-            drawMatrix.preTranslate(-centerX, -centerY)
-            drawMatrix.postTranslate(centerX, centerY)
-            //高度变矮
-            canvas?.drawBitmap(this, drawMatrix, null)
-            canvas?.restore()
-        }
-    }
-
-    /*
-    * 绘制下半部分，以及下半部分的变化
-    * 下半部分由0 变化到 90，递增
-    * */
-    fun drawSecondHalf(canvas: Canvas?, bitmap: Bitmap?, rotate: Float) {
-        bitmap?.apply {
-            canvas?.save()
-            camera.save()
-            canvas?.clipRect(0, centerY.toInt(), width, height)
-            camera.rotateX(0f)
-            camera.getMatrix(drawMatrix)
-            camera.restore()
-            drawMatrix.preScale(1.0f, 1f)
-            drawMatrix.preTranslate(-centerX, -centerY)
-            drawMatrix.postTranslate(centerX, centerY)
-            //高度变矮
-
             canvas?.drawBitmap(this, drawMatrix, null)
             canvas?.restore()
         }
--- app/src/main/java/com/example/jadynai/loadinglovely/flipboard/FlipActivity.kt
+++ app/src/main/java/com/example/jadynai/loadinglovely/flipboard/FlipActivity.kt
@@ -3,12 +3,23 @@ package com.example.jadynai.loadinglovely.flipboard
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.example.jadynai.loadinglovely.R
+import kotlinx.android.synthetic.main.activity_flip.*
 
 class FlipActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_flip)
-        
+        pre_tv.setOnClickListener {
+            debug_view.drawMatrix.postRotate(45f)
+            debug_view.drawMatrix.preTranslate(-debug_view.centerX, -debug_view.centerY)
+            debug_view.drawMatrix.postTranslate(debug_view.centerX, debug_view.centerY)
+            debug_view.invalidate()
+        }
+        post_tv.setOnClickListener {
+            debug_view.drawMatrix.reset()
+            debug_view.drawMatrix.postRotate(45f)
+            debug_view.invalidate()
+        }
     }
 }
