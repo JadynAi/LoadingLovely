fa1203a48d794878618a4ad50ec66199cf89ce4c
jadynai <936745006@qq.com>
2019-04-23 10:02:02 +0800

    1.0版本 arc下拉布局新算法提交

--- app/src/main/java/com/example/jadynai/loadinglovely/pullarc/ArcContainer.java
+++ app/src/main/java/com/example/jadynai/loadinglovely/pullarc/ArcContainer.java
@@ -6,8 +6,6 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PointF;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffXfermode;
 import android.util.AttributeSet;
 import android.widget.RelativeLayout;
 
@@ -24,19 +22,13 @@ public class ArcContainer extends RelativeLayout {
 
     Path mClipPath;
 
-    int width = 0;
-    int height = 0;
-
     Paint mPaint;
 
-    private PorterDuffXfermode porterDuffXfermode;
+    private int mOriginalHeight;
 
-    private int orignalHeight;
-
-    private boolean firstGetHeight = true;
-
-    private PointF p0;
-    private PointF p2;
+    private PointF mPointStart;
+    private PointF mPointEnd;
+    private PointF mPointMid;
 
     public ArcContainer(Context context) {
         super(context);
@@ -50,10 +42,8 @@ public class ArcContainer extends RelativeLayout {
 
     private void init(Context context, AttributeSet attrs) {
         mContext = context;
-
-        porterDuffXfermode = new PorterDuffXfermode(PorterDuff.Mode.CLEAR);
-
         mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setStyle(Paint.Style.FILL);
         mPaint.setColor(Color.WHITE);
         mClipPath = new Path();
     }
@@ -62,53 +52,36 @@ public class ArcContainer extends RelativeLayout {
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
-        width = getMeasuredWidth();
-        height = getMeasuredHeight();
-
-
-        if (firstGetHeight) {
-            orignalHeight = height;
-            firstGetHeight = false;
+        if (mOriginalHeight == 0) {
+            mOriginalHeight = getMeasuredHeight();
+            mPointStart = new PointF(0, mOriginalHeight);
+            mPointEnd = new PointF(getMeasuredWidth(), mOriginalHeight);
+            mPointMid = new PointF(getMeasuredWidth(), mOriginalHeight);
         }
-        mClipPath = getClipPath(width, height, orignalHeight);
+        mClipPath.reset();
+        mClipPath.moveTo(0, mOriginalHeight);
+        mPointMid.y = getMeasuredHeight();
+        mClipPath.cubicTo(0, mOriginalHeight, getMeasuredWidth() / 2,
+                calculateBezierPeak(0, mOriginalHeight,
+                        getMeasuredWidth() / 2, getMeasuredHeight(), getMeasuredWidth(), mOriginalHeight)[1],
+                getMeasuredWidth(), mOriginalHeight);
+        mClipPath.lineTo(getMeasuredWidth(), getHeight());
+        mClipPath.lineTo(0, getHeight());
+        mClipPath.close();
     }
 
     @Override
     protected void dispatchDraw(Canvas canvas) {
-
         int saveCount = canvas.saveLayer(0, 0, getMeasuredWidth(), getMeasuredHeight(), null, Canvas.ALL_SAVE_FLAG);
         super.dispatchDraw(canvas);
-        mPaint.setXfermode(porterDuffXfermode);
         canvas.drawPath(mClipPath, mPaint);
-        mPaint.setXfermode(null);
         canvas.restoreToCount(saveCount);
     }
 
-    Path getClipPath(int width, int height, int orignalHeight) {
-
-        Path path = new Path();
-
-        path.moveTo(0, orignalHeight);
-        path.cubicTo(0, orignalHeight, width / 2, orignalHeight + 2.2f * (height - orignalHeight), width, orignalHeight);
-        path.lineTo(width, height);
-        path.lineTo(0, height);
-        path.close();
-
-        return path;
-    }
-
-    /**
-     * @param p0 起始点
-     * @param p1 曲线顶点
-     * @param p2 终止点
-     * @return t对应的点
-     */
-    private PointF calculateBezierControlPoint(PointF p0, PointF p1, PointF p2) {
-        PointF point = new PointF();
-        // 曲线运动到的比例，0-1之间。因为这里起始点和终点配合成为等腰三角形，所以使用0.5这里表示运动到一半
+    private float[] calculateBezierPeak(int startX, int startY, int midX, int midY, int endX, int endY) {
         float temp = 0.5f;
-        point.x = (float) ((p1.x - Math.pow(temp, 2) * (p0.x + p2.x)) / (2 * Math.pow(temp, 2)));
-        point.y = (float) ((p1.y - Math.pow(temp, 2) * (p0.y + p2.y)) / (2 * Math.pow(temp, 2)));
-        return point;
+        float x = (float) ((midX - Math.pow(temp, 2) * (startX + endX)) / (2 * Math.pow(temp, 2)));
+        float y = (float) ((midY - Math.pow(temp, 2) * (startY + endY)) / (2 * Math.pow(temp, 2)));
+        return new float[] {x, y};
     }
 }
